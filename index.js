809b5143e7c20348cb9ec31269e0110d1a29b00f
Philipp <prath@bbw-fi.de>
2023-08-09 14:18:04 +0200

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import Blog from './models/Blog.js';
 
-const url = "mongodb+srv://prath:wpOFkHG2elRfYVF8@bbwfi.m1wu7rg.mongodb.net/Praktikumsbörse"
+
 
 await mongoose.connect(
-    url
+    process.env.MONGO_URI || 'mongodb://localhost:27017/blog',
 )
 
 const article = new Blog({
